1b0a5dfb788ee2b216350a92404aaee85151b533
Dwijendra Parashar <dwijendraparashartech@gmail.com>
2020-12-07 18:48:18 +0530

    add

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const PORT = 5000
+mongoose.connect()
+const {MONGOURI} = require('./keys')
+require('./models/user')
+
+app.use(require('./routes/auth'))
+app.use(express.json())
+mongoose.model("User")
+
+
+
+mongoose.connect(MONGOURI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+mongoose.connection.on('connected',()=>{
+    console.log("connected on mongo yeahh")
+
+})
+
+mongoose.connection.on('error',(err)=>{
+    console.log("err connecting",err)
+
+})
+
+
+// const customMiddleware =(req,res,next) => {
+//     console.log("middleware executed!")
+//     next()  
+// }
+
+// app.use(customMiddleware)
+
+
+// app.get('/',(req,res)=>{
+//   console.log("home")
+//   res.send("hello world")
+// })
+
+// app.get('/about',customMiddleware,(req,res)=>{
+//     console.log("about")
+//     res.send("about page")
+//   })
+
+app.listen(PORT,()=>{
+    console.log("server is running on",PORT)
+})
+
+
+
---
+++ keys.js
@@ -0,0 +1,3 @@
+module.exports={
+    NONGOURI:"mongodb+srv://dwijendra:LmydE0PecdOUBEel@cluster0.hhlnt.mongodb.net/<dbname>?retryWrites=true&w=majority"
+}
