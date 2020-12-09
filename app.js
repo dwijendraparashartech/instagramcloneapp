dea2b695d1fed0c3d3490bacd06a200340aef236
Dwijendra Parashar <dwijendraparashartech@gmail.com>
2020-12-09 18:46:33 +0530

    post_schema_and_create_routes

--- app.js
+++ app.js
@@ -2,14 +2,9 @@ const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
 const PORT = 5000
-// mongoose.connect()
 const {MONGOURI} = require('./keys')
-require('./models/user')
 
-app.use(express.json())
-app.use(require('./routes/auth'))
 
-mongoose.model("User")
 
 
 
@@ -28,6 +23,12 @@ mongoose.connection.on('error',(err)=>{
 
 })
 
+require('./models/user')
+require('./models/post')
+
+app.use(express.json())
+app.use(require('./routes/auth'))
+app.use(require('./routes/post'))
 
 // const customMiddleware =(req,res,next) => {
 //     console.log("middleware executed!")
--- keys.js
+++ keys.js
@@ -1,5 +1,6 @@
 module.exports={
-    MONGOURI:"mongodb+srv://dwijendra:LmydE0PecdOUBEel@cluster0.hhlnt.mongodb.net/<dbname>?retryWrites=true&w=majority"
+    MONGOURI:"mongodb+srv://dwijendra:LmydE0PecdOUBEel@cluster0.hhlnt.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    JWT_SECRET:"efgdkgdgkdf45"
 }
 
 
