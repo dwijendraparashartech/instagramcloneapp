e3740a7028f140371879e43f5cc5d10c14fc8c45
Dwijendra Parashar <dwijendraparashartech@gmail.com>
2020-12-11 17:33:02 +0530

    serverend

--- routes/post.js
+++ routes/post.js
@@ -5,15 +5,29 @@ const mongoose = require('mongoose')
 const requireLogin  = require('../middleware/requireLogin')
 const Post = mongoose.model("Post")
 
+
+
+router.get('/allpost',(req,res)=>{
+    Post.find()
+    .populate("postedBy","_id name")
+    .then(posts=>{
+        res.json({posts})
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+    })
+
 router.post('/createpost',requireLogin,(req,res)=>{
-    const {title,body} = req.body
-    if(!title||!body){
+    const {title,body,pic} = req.body
+    if(!title||!body||!pic){
        return res.status(422).json({error:'please add all the fields'})
     }
     req.user.password = undefined
     const post=new Post ({
     title,
     body,
+    pic,
     postedBy:req.user
 })
     post.save().then(result=>{
@@ -24,4 +38,14 @@ router.post('/createpost',requireLogin,(req,res)=>{
     })
 })
 
+router.get('/mypost',requireLogin,(req,res)=>{
+    Post.find({postedBy:req.user._id})
+    .populate("PostedBy","_id name")
+    .then(mypost=>{
+        res.json({mypost})
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+})
 module.exports = router 
