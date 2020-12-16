2087d730a3dbf949ba160f30eb1d65eb1a9faea2
Dwijendra Parashar <dwijendraparashartech@gmail.com>
2020-12-16 19:14:45 +0530

    requiredLogginError

--- middleware/requireLogin.js
+++ middleware/requireLogin.js
@@ -6,10 +6,12 @@ const User = mongoose.model("User")
 module.exports=(req,res,next)=>{
  const {authorization} = req.headers
  //authorization === Bearer raafdkfkjkgke
+
  if(!authorization){
     return res.status(401).json({error:"you must be logged in"})
  }
  const token= authorization.replace("Bearer","")
+
  jwt.verify(token,JWT_SECRET,(err,payload)=>{
      if(err){
          return res.status(401).json({error:"you must be logged in "})
